a85ec137128db32a2d6ace4bc1ffccf98fd3176b
Masih Moloodian <masihmoloodian@gmail.com>
2023-08-28 10:20:26 +0330

    - feat: Add user entity with hash password using bcrypt

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import { ParentEntity } from '../shared/entities/parent.entity';
+import { hash } from 'bcrypt';
+
+@Entity('users')
+export class UserEntity extends ParentEntity {
+    constructor(entity?: Partial<UserEntity>) {
+        super()
+        this.setArgumentToThisObject(entity)
+    }
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @BeforeInsert()
+    async hashPassword() {
+        if (this.password) {
+            this.password = await hash(this.password, process.env.SALT_HASH)
+        }
+    }
+}
