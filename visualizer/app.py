bb5747b5860bf536ce505ee19315e80e8f19c76c
Masih Moloodian <masihmoloodian@gmail.com>
2023-08-30 07:42:50 +0330

    - Add TOKEN variable

--- visualizer/app.py
+++ visualizer/app.py
@@ -1,4 +1,3 @@
-import random
 import streamlit as st
 import folium
 import streamlit as st
@@ -12,13 +11,14 @@ st.set_page_config(
 )
 st.session_state.keep_graphics = True
 
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2ZjAyNzM5LTU2NGUtNGVmZS1iMmIzLTVkNzQwNzUwMmI4YSIsInVzZXJuYW1lIjoibWFzaWhtb2xvb2RpYW5AZ21haWwuY29tIiwiaWF0IjoxNjkzMzY3ODU4LCJleHAiOjE2OTM0NTQyNTh9.HbMuf1eTKoWbIQdIbp9LNbmgY0gSRL_vOYrxKudvVeA"
+
 url = 'http://localhost:3000/point'
 headers = {
     'accept': '*/*',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2ZjAyNzM5LTU2NGUtNGVmZS1iMmIzLTVkNzQwNzUwMmI4YSIsInVzZXJuYW1lIjoibWFzaWhtb2xvb2RpYW5AZ21haWwuY29tIiwiaWF0IjoxNjkzMzY3ODU4LCJleHAiOjE2OTM0NTQyNTh9.HbMuf1eTKoWbIQdIbp9LNbmgY0gSRL_vOYrxKudvVeA'
+    'Authorization': f'Bearer {TOKEN}'
 }
 response = requests.get(url, headers=headers)
-
 if response.status_code == 200:
     response = response.json()
 
@@ -29,9 +29,7 @@ for point in response:
         "lon": point['point']['coordinates'][1],
         "name": point['name'],
     })
-
 m = folium.Map(zoom_start=2)
-
 for point in points:
     folium.Marker([point["lat"], point["lon"]], popup=point["name"]).add_to(m)
 st_data = st_folium(m,width=725)
